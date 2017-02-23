d02d00a6f868957c6f40dddb56876fe9612d4542
josephsap <joesap1@gmail.com>
2017-02-23 16:53:28 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    imagemin = require('gulp-imagemin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    watch = require('gulp-watch');
+
+// make vars for file paths
+var paths = {
+  source: 'src/assets/',
+  dest: 'build/',
+  'js': {
+    'bootstrap': [
+      './src/assets/js/bootstrap/affix.js',
+      './src/assets/js/bootstrap/alert.js',
+      './src/assets/js/bootstrap/offcanvas.js'
+    ]
+  }
+};
+
+gulp.task('bootstrapJS', function() {
+  gulp.src(paths.js.bootstrap)
+    .pipe(concat('bootstrap.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest + 'js'))
+    .pipe(connect.reload())
+});
+
+gulp.task('scripts', function() {
+  gulp.src(paths.source + 'js/*.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest + 'js'))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  gulp.src('**/*.html')
+  .pipe(connect.reload())
+});
+
+// compress images
+gulp.task('smush', function() {
+  gulp.src('src/assets/images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('build/images'))
+});
+
+// minify css
+gulp.task('sass', function() {
+  return gulp.src(paths.source + 'scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      browsers: ['defaults', 'last 2 versions', 'Firefox > 45', '> 1% in US'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(paths.dest + '/css'))
+});
+
+// watch for changes in css and js then compile
+gulp.task('watch', function() {
+  gulp.watch(paths.source + 'scss/**/*.scss', ['sass']);
+  gulp.watch(paths.source + 'js/*.js', ['scripts']);
+  gulp.watch('**/*.html', ['html']);
+});
+
+gulp.task('default', ['sass', 'bootstrapJS', 'scripts', 'connect', 'watch']);
---
+++ src/assets/js/main.js
@@ -0,0 +1,34 @@
+var Main = {
+
+  init: function() {
+    var _this = this;
+    this.$window = $(window);
+    this.$window.on('scroll', function() {
+      _this.navShrink();
+    });
+    this.sidebarNav();
+  },
+
+  navShrink: function() {
+
+    // if you scroll up 60px, shrink the nav. scroll down 60px, return nav to regular height
+    var $nav = $('.navbar-fixed-top');
+    this.$window.scrollTop() > 60 ? $nav.addClass('shrink') : $nav.removeClass('shrink');
+  },
+
+  sidebarNav: function() {
+    var sideslider = $('[data-toggle=collapse-side]');
+    var sel = sideslider.attr('data-target');
+    var sel2 = sideslider.attr('data-target-2');
+    sideslider.click(function(event){
+        $(sel).toggleClass('in');
+        $(sel2).toggleClass('out');
+    });
+  }
+
+};
+
+
+(function() {
+  Main.init();
+})();
