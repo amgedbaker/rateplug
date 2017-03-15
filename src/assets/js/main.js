e644d5799ecb39c4a7fb5d7f82f25221e82fbe6d
josephsap <joesap1@gmail.com>
2017-03-15 11:52:26 -0600

    qa fixes started

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -54,19 +54,22 @@ var Main = {
     $('.carousel').carousel();
     // for every slide in carousel, copy the next slide's item in the slide.
     // Do the same for the next, next item.
-    $('.carousel .item').each(function(){
-      var next = $(this).next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-      
-      if (next.next().length>0) {
-        next.next().children(':first-child').clone().appendTo($(this));
-      } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-      }
-    });
+
+    if($(window).width() >= 768) {
+      $('.carousel .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        if (next.next().length>0) {
+          next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+          $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+      });
+    }
   },
 
   dropdown: function() {
